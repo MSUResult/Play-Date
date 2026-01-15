e770ca5689bc382f60c03226f01def781576d2ab
MSUResult <adarshrajput9040@gmail.com>
2026-01-15 09:34:32 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // ✅ THIS LINE
+  },
 };
 
 export default nextConfig;
